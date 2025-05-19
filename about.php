7a98d12ac77270404be19cf75b9f6e1a837cd25b
Hanisa <113896007+AanChan07@users.noreply.github.com>
2025-05-20 00:28:45 +0700

    Update about.php

--- about.php
+++ about.php
@@ -15,8 +15,30 @@
         <div class="div">
             <?php include 'navbar.php'; ?>
 
+            <div class="atas">
+                <div class="text-nav">ABOUT US</div>
+            </div>
 
-
+            <div class="form">
+                <div class="contact">
+                    <div class="rectangle-2"></div>
+                    <img class="gambar_aboutus" src="img/aboutus.png" />
+                    <p class="aboutus">
+                        <span class="text-aboautus"> Selamat dtang di</span>
+                        <span class="text-aboautus-1">Hotel Del Luna</span>
+                        <span class="text-aboautus">, tempat nyaman untuk setiap perjalanan Anda. Kami hadir untuk
+                            memberikan pengalaman menginap yang tak terlupakan, dengan keindahan pantai, hangatnya
+                            matahari terbenam, dan magisnya cahaya bulan berpadu menciptakan suasana yang menenangkan
+                            jiwa.<br /><br />Terletak di tepi laut dengan pemandangan langsung ke horizon, Hotel Del
+                            Luna menawarkan pengalaman menginap yang tak hanya nyaman, tapi juga memikat hati. Setiap
+                            sore, Anda bisa menikmati senja berwarna jingga dari balkon kamar, sementara malam hari
+                            membawa nuansa tenang di bawah sinar bulan.<br /><br />Kami percaya, liburan yang sempurna
+                            dimulai dari tempat yang penuh ketenangan dan keindahan. Itulah mengapa kami menyediakan
+                            fasilitas lengkap seperti restoran, kolam renang, layanan pelanggan 24 jam, dan suasana
+                            tropis yang hangat. Semua Hanya Untuk Anda.</span>
+                    </p>
+                </div>
+            </div>
 
             <?php include 'footer.php'; ?>
         </div>
