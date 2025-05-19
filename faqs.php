2d1ecb03cc7560a6bab3a74eef8b29140cee9406
Hanisa <113896007+AanChan07@users.noreply.github.com>
2025-05-19 22:26:21 +0700

    Update faqs.php

--- faqs.php
+++ faqs.php
@@ -15,7 +15,23 @@
         <div class="div">
             <?php include 'navbar.php'; ?>
 
-
+            <div class="atas">
+                <div class="text-nav">FAQS</div>
+            </div>
+            <div class="form">
+                <div class="contact">
+                    <div class="rectangle-2"></div>
+                    <div class="list">
+                        <p class="text-message-2"> Q: Bagaimana cara booking kamarnya?</p>
+                        <p class="text-message-3"> A: Pertama anda harus mendaftar terlebih dahulu sebagai pengguna
+                            melalui menu registrasi</p>
+                        <p class="text-message-4"> Q: Apakah hotel del luna ini menyediakan fasilitas untuk berolahraga
+                        </p>
+                        <p class="text-message-5"> A: Ya, kami menyediakan fasilitas fitness studio yang dapat
+                            digunakanoleh pengunjung</p>
+                    </div>
+                </div>
+            </div>
 
 
             <?php include 'footer.php'; ?>
