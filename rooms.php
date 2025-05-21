a0ba1446df0bc6571ec435861b6735ca172d4051
Hanisa <113896007+AanChan07@users.noreply.github.com>
2025-05-21 19:35:20 +0700

    Update rooms.php

--- rooms.php
+++ rooms.php
@@ -6,6 +6,8 @@
     <meta charset="utf-8" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.2.0/remixicon.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+
     <link rel="stylesheet" href="css/globals.css" />
     <link rel="stylesheet" href="css/style.css" />
 </head>
@@ -15,12 +17,330 @@
         <div class="div">
             <?php include 'navbar.php'; ?>
 
+            <div class="atas">
+                <div class="text-nav">ROOMS</div>
+            </div>
 
+            <div class="box-rooms">
+                <div class="room1">
+                    <div class="overlap-rooms11">
+                        <div class="tombol-booking1">
+                            <a href="detail_rooms.php" class="text-booking-1">Booking Sekarang</a>
+                        </div>
+                        <div class="jenis-rooms-1">Deluxe King</div>
+                        <div class="harga1">RP 850.000/Night</div>
+                    </div>
+                    <div class="overlap-rooms12">
+                        <img class="gambar-rooms-1" src="img/deluxe.png" />
+                        <div class="rectangle-rooms-1"></div>
+                        <div class="jumlah1">
+                            <div class="text-jumlah-1">2</div>
+                            <i class="fa-solid fa-user gambar-jumlah-1"></i>
+                        </div>
+                        <div class="fasilitas1">
+                            <div class="text-fasilitas-1">Balcony With Sunset</div>
+                            <i class="fas fa-square-check gambar-fasilitas-1"></i>
+                        </div>
+                        <div class="bad1">
+                            <i class="fa-solid fa-bed gambar-jumlah-1"></i>
+                            <div class="text-bad-1">1 King Bed</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="room2">
+                    <div class="overlap-rooms11">
+                        <div class="tombol-booking1">
+                            <a href="detail_rooms.php" class="text-booking-1">Booking Sekarang</a>
+                        </div>
+                        <div class="jenis-rooms-1">Deluxe Twin</div>
+                        <div class="harga1">RP 850.000/Night</div>
+                    </div>
+                    <div class="overlap-rooms12">
+                        <img class="gambar-rooms-1" src="img/deluxe_twin.png" />
+                        <div class="rectangle-rooms-1"></div>
+                        <div class="jumlah1">
+                            <div class="text-jumlah-1">2</div>
+                            <i class="fa-solid fa-user gambar-jumlah-1"></i>
+                        </div>
+                        <div class="fasilitas1">
+                            <div class="text-fasilitas-1">Mini Bar</div>
+                            <i class="fas fa-square-check gambar-fasilitas-1"></i>
+                        </div>
+                        <div class="bad1">
+                            <i class="fa-solid fa-bed gambar-jumlah-1"></i>
+                            <div class="text-bad-1">2 Single Bed</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="room3">
+                    <div class="overlap-rooms11">
+                        <div class="tombol-booking1">
+                            <a href="detail_rooms.php" class="text-booking-1">Booking Sekarang</a>
+                        </div>
+                        <div class="jenis-rooms-1">Deluxe Triple</div>
+                        <div class="harga1">RP 950.000/Night</div>
+                    </div>
+                    <div class="overlap-rooms12">
+                        <img class="gambar-rooms-1" src="img/deluxe_triple.png" />
+                        <div class="rectangle-rooms-1"></div>
+                        <div class="jumlah1">
+                            <div class="text-jumlah-1">3</div>
+                            <i class="fa-solid fa-user gambar-jumlah-1"></i>
+                        </div>
+                        <div class="fasilitas1">
+                            <div class="text-fasilitas-1">LCD TV</div>
+                            <i class="fas fa-square-check gambar-fasilitas-1"></i>
+                        </div>
+                        <div class="bad1">
+                            <i class="fa-solid fa-bed gambar-jumlah-1"></i>
+                            <div class="text-bad-1">Single and king bad</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="room4">
+                    <div class="overlap-rooms11">
+                        <div class="tombol-booking1">
+                            <a href="detail_rooms.php" class="text-booking-1">Booking Sekarang</a>
+                        </div>
+                        <div class="jenis-rooms-1">Cabin Twin</div>
+                        <div class="harga1">RP 750.000/Night</div>
+                    </div>
+                    <div class="overlap-rooms12">
+                        <img class="gambar-rooms-1" src="img/cabintwin.png" />
+                        <div class="rectangle-rooms-1"></div>
+                        <div class="jumlah1">
+                            <div class="text-jumlah-1">2</div>
+                            <i class="fa-solid fa-user gambar-jumlah-1"></i>
+                        </div>
+                        <div class="fasilitas1">
+                            <div class="text-fasilitas-1">Balcony With Sunset</div>
+                            <i class="fas fa-square-check gambar-fasilitas-1"></i>
+                        </div>
+                        <div class="bad1">
+                            <i class="fa-solid fa-bed gambar-jumlah-1"></i>
+                            <div class="text-bad-1">2 Single Bed</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="room5">
+                    <div class="overlap-rooms11">
+                        <div class="tombol-booking1">
+                            <a href="detail_rooms.php" class="text-booking-1">Booking Sekarang</a>
+                        </div>
+                        <div class="jenis-rooms-1">Cabin Queen</div>
+                        <div class="harga1">RP 750.000/Night</div>
+                    </div>
+                    <div class="overlap-rooms12">
+                        <img class="gambar-rooms-1" src="img/CabinQueen.png" />
+                        <div class="rectangle-rooms-1"></div>
+                        <div class="jumlah1">
+                            <div class="text-jumlah-1">2</div>
+                            <i class="fa-solid fa-user gambar-jumlah-1"></i>
+                        </div>
+                        <div class="fasilitas1">
+                            <div class="text-fasilitas-1">Balcony With Sunset</div>
+                            <i class="fas fa-square-check gambar-fasilitas-1"></i>
+                        </div>
+                        <div class="bad1">
+                            <i class="fa-solid fa-bed gambar-jumlah-1"></i>
+                            <div class="text-bad-1">2 Single Bed</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="room6">
+                    <div class="overlap-rooms11">
+                        <div class="tombol-booking1">
+                            <a href="detail_rooms.php" class="text-booking-1">Booking Sekarang</a>
+                        </div>
+                        <div class="jenis-rooms-1">Executive Suite</div>
+                        <div class="harga1">RP 1250.000/Night</div>
+                    </div>
+                    <div class="overlap-rooms12">
+                        <img class="gambar-rooms-1" src="img/ExecutiveSuite.png" />
+                        <div class="rectangle-rooms-1"></div>
+                        <div class="jumlah1">
+                            <div class="text-jumlah-1">2</div>
+                            <i class="fa-solid fa-user gambar-jumlah-1"></i>
+                        </div>
+                        <div class="fasilitas1">
+                            <div class="text-fasilitas-1">Balcony With Sunset</div>
+                            <i class="fas fa-square-check gambar-fasilitas-1"></i>
+                        </div>
+                        <div class="bad1">
+                            <i class="fa-solid fa-bed gambar-jumlah-1"></i>
+                            <div class="text-bad-1">2 Single Bed</div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="room7">
+                    <div class="overlap-rooms11">
+                        <div class="tombol-booking1">
+                            <a href="detail_rooms.php" class="text-booking-1">Booking Sekarang</a>
+                        </div>
+                        <div class="jenis-rooms-1">Executive Suite</div>
+                        <div class="harga1">RP 1250.000/Night</div>
+                    </div>
+                    <div class="overlap-rooms12">
+                        <img class="gambar-rooms-1" src="img/ExecutiveSuite.png" />
+                        <div class="rectangle-rooms-1"></div>
+                        <div class="jumlah1">
+                            <div class="text-jumlah-1">2</div>
+                            <i class="fa-solid fa-user gambar-jumlah-1"></i>
+                        </div>
+                        <div class="fasilitas1">
+                            <div class="text-fasilitas-1">Balcony With Sunset</div>
+                            <i class="fas fa-square-check gambar-fasilitas-1"></i>
+                        </div>
+                        <div class="bad1">
+                            <i class="fa-solid fa-bed gambar-jumlah-1"></i>
+                            <div class="text-bad-1">2 Single Bed</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="room8">
+                    <div class="overlap-rooms11">
+                        <div class="tombol-booking1">
+                            <a href="detail_rooms.php" class="text-booking-1">Booking Sekarang</a>
+                        </div>
+                        <div class="jenis-rooms-1">Executive Suite</div>
+                        <div class="harga1">RP 1250.000/Night</div>
+                    </div>
+                    <div class="overlap-rooms12">
+                        <img class="gambar-rooms-1" src="img/ExecutiveSuite.png" />
+                        <div class="rectangle-rooms-1"></div>
+                        <div class="jumlah1">
+                            <div class="text-jumlah-1">2</div>
+                            <i class="fa-solid fa-user gambar-jumlah-1"></i>
+                        </div>
+                        <div class="fasilitas1">
+                            <div class="text-fasilitas-1">Balcony With Sunset</div>
+                            <i class="fas fa-square-check gambar-fasilitas-1"></i>
+                        </div>
+                        <div class="bad1">
+                            <i class="fa-solid fa-bed gambar-jumlah-1"></i>
+                            <div class="text-bad-1">2 Single Bed</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="room9">
+                    <div class="overlap-rooms11">
+                        <div class="tombol-booking1">
+                            <a href="detail_rooms.php" class="text-booking-1">Booking Sekarang</a>
+                        </div>
+                        <div class="jenis-rooms-1">Executive Suite</div>
+                        <div class="harga1">RP 1250.000/Night</div>
+                    </div>
+                    <div class="overlap-rooms12">
+                        <img class="gambar-rooms-1" src="img/ExecutiveSuite.png" />
+                        <div class="rectangle-rooms-1"></div>
+                        <div class="jumlah1">
+                            <div class="text-jumlah-1">2</div>
+                            <i class="fa-solid fa-user gambar-jumlah-1"></i>
+                        </div>
+                        <div class="fasilitas1">
+                            <div class="text-fasilitas-1">Balcony With Sunset</div>
+                            <i class="fas fa-square-check gambar-fasilitas-1"></i>
+                        </div>
+                        <div class="bad1">
+                            <i class="fa-solid fa-bed gambar-jumlah-1"></i>
+                            <div class="text-bad-1">2 Single Bed</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="room10">
+                    <div class="overlap-rooms11">
+                        <div class="tombol-booking1">
+                            <a href="detail_rooms.php" class="text-booking-1">Booking Sekarang</a>
+                        </div>
+                        <div class="jenis-rooms-1">Executive Suite</div>
+                        <div class="harga1">RP 1250.000/Night</div>
+                    </div>
+                    <div class="overlap-rooms12">
+                        <img class="gambar-rooms-1" src="img/ExecutiveSuite.png" />
+                        <div class="rectangle-rooms-1"></div>
+                        <div class="jumlah1">
+                            <div class="text-jumlah-1">2</div>
+                            <i class="fa-solid fa-user gambar-jumlah-1"></i>
+                        </div>
+                        <div class="fasilitas1">
+                            <div class="text-fasilitas-1">Balcony With Sunset</div>
+                            <i class="fas fa-square-check gambar-fasilitas-1"></i>
+                        </div>
+                        <div class="bad1">
+                            <i class="fa-solid fa-bed gambar-jumlah-1"></i>
+                            <div class="text-bad-1">2 Single Bed</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="room11">
+                    <div class="overlap-rooms11">
+                        <div class="tombol-booking1">
+                            <a href="detail_rooms.php" class="text-booking-1">Booking Sekarang</a>
+                        </div>
+                        <div class="jenis-rooms-1">Executive Suite</div>
+                        <div class="harga1">RP 1250.000/Night</div>
+                    </div>
+                    <div class="overlap-rooms12">
+                        <img class="gambar-rooms-1" src="img/ExecutiveSuite.png" />
+                        <div class="rectangle-rooms-1"></div>
+                        <div class="jumlah1">
+                            <div class="text-jumlah-1">2</div>
+                            <i class="fa-solid fa-user gambar-jumlah-1"></i>
+                        </div>
+                        <div class="fasilitas1">
+                            <div class="text-fasilitas-1">Balcony With Sunset</div>
+                            <i class="fas fa-square-check gambar-fasilitas-1"></i>
+                        </div>
+                        <div class="bad1">
+                            <i class="fa-solid fa-bed gambar-jumlah-1"></i>
+                            <div class="text-bad-1">2 Single Bed</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="room12">
+                    <div class="overlap-rooms11">
+                        <div class="tombol-booking1">
+                            <a href="detail_rooms.php" class="text-booking-1">Booking Sekarang</a>
+                        </div>
+                        <div class="jenis-rooms-1">Executive Suite</div>
+                        <div class="harga1">RP 1250.000/Night</div>
+                    </div>
+                    <div class="overlap-rooms12">
+                        <img class="gambar-rooms-1" src="img/ExecutiveSuite.png" />
+                        <div class="rectangle-rooms-1"></div>
+                        <div class="jumlah1">
+                            <div class="text-jumlah-1">2</div>
+                            <i class="fa-solid fa-user gambar-jumlah-1"></i>
+                        </div>
+                        <div class="fasilitas1">
+                            <div class="text-fasilitas-1">Balcony With Sunset</div>
+                            <i class="fas fa-square-check gambar-fasilitas-1"></i>
+                        </div>
+                        <div class="bad1">
+                            <i class="fa-solid fa-bed gambar-jumlah-1"></i>
+                            <div class="text-bad-1">2 Single Bed</div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
 
 
             <?php include 'footer.php'; ?>
         </div>
     </div>
+
 
 
 </body>
