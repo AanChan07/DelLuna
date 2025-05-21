5f1d4d99ae8a2f20dac04318eae90eea0148a075
Hanisa <113896007+AanChan07@users.noreply.github.com>
2025-05-21 19:36:09 +0700

    Update facilities.php

--- facilities.php
+++ facilities.php
@@ -15,7 +15,102 @@
         <div class="div">
             <?php include 'navbar.php'; ?>
 
+            <div class="atas">
+                <div class="text-nav">FASILITIES</div>
+            </div>
 
+            <div class="box-fasilitas">
+                <div class="fasilities1">
+                    <div class="overlap-fasilitas">
+                        <img class="gambar-fasilities" src="img/pantai.png" />
+                        <div class="rectangle-fasilitas"></div>
+                        <div class="jenis-fasilitas">
+                            <div class="text-jenis-fasilitas">Kolam Renang Cakrawala</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="fasilities2">
+                    <div class="overlap-fasilitas">
+                        <img class="gambar-fasilities" src="img/Rooftop.png" />
+                        <div class="rectangle-fasilitas"></div>
+                        <div class="jenis-fasilitas">
+                            <div class="text-jenis-fasilitas">Rooftop Bar & Grill</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="fasilities3">
+                    <div class="overlap-fasilitas">
+                        <img class="gambar-fasilities" src="img/kolam.png" />
+                        <div class="rectangle-fasilitas"></div>
+                        <div class="jenis-fasilitas">
+                            <div class="text-jenis-fasilitas">Kolam Renang Cakrawala</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="fasilities4">
+                    <div class="overlap-fasilitas">
+                        <img class="gambar-fasilities" src="img/resto.png" />
+                        <div class="rectangle-fasilitas"></div>
+                        <div class="jenis-fasilitas">
+                            <div class="text-jenis-fasilitas">Restoran</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="fasilities5">
+                    <div class="overlap-fasilitas">
+                        <img class="gambar-fasilities" src="img/gym.png" />
+                        <div class="rectangle-fasilitas"></div>
+                        <div class="jenis-fasilitas">
+                            <div class="text-jenis-fasilitas">Studio Kebugaran</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="fasilities6">
+                    <div class="overlap-fasilitas">
+                        <img class="gambar-fasilities" src="img/pertemuan.png" />
+                        <div class="rectangle-fasilitas"></div>
+                        <div class="jenis-fasilitas">
+                            <div class="text-jenis-fasilitas">Ruang Pertemuan</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="fasilities7">
+                    <div class="overlap-fasilitas">
+                        <img class="gambar-fasilities" src="img/sarapan.png" />
+                        <div class="rectangle-fasilitas"></div>
+                        <div class="jenis-fasilitas">
+                            <div class="text-jenis-fasilitas">Ruang Sarapan</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="fasilities8">
+                    <div class="overlap-fasilitas">
+                        <img class="gambar-fasilities" src="img/sepeda.png" />
+                        <div class="rectangle-fasilitas"></div>
+                        <div class="jenis-fasilitas">
+                            <div class="text-jenis-fasilitas">Tersedia Sepeda</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="fasilities9">
+                    <div class="overlap-fasilitas">
+                        <img class="gambar-fasilities" src="img/main.png" />
+                        <div class="rectangle-fasilitas"></div>
+                        <div class="jenis-fasilitas">
+                            <div class="text-jenis-fasilitas">Area Bermain</div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
 
 
             <?php include 'footer.php'; ?>
